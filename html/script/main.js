81e31d22e0972c7aebeef650dd1ea0b66f1f3e29
RedstoneParadox <spinjitzu00@gmail.com>
2024-03-05 22:14:08 -0800

    Footer tag

--- html/script/main.js
+++ html/script/main.js
@@ -1,7 +1,7 @@
 function toggleDarkMode() {
   var header_top = document.querySelector(".header-top")
   var header_nav = document.querySelector(".header-nav")
-  var footer = document.querySelector("#footer")
+  var footer = document.querySelector(".footer")
   var body = document.body
   var nav_links  = document.querySelectorAll(".nav-link")
 
