6d2905a56a871eab9d92e2f52ca48b02559dc314
RedstoneParadox <spinjitzu00@gmail.com>
2024-03-19 09:29:52 -0700

    Better theme icons

--- html/script/main.js
+++ html/script/main.js
@@ -3,8 +3,8 @@ function toggleDarkMode() {
   var themeIcon = document.querySelector(".theme-icon")
 
   if (!document.body.classList.contains("dark")) {
-    themeIcon.src = "images/button/light_mode_FILL0_wght400_GRAD0_opsz24.svg"
+    themeIcon.src = "images/theme/sun-fill.svg"
   } else {
-    themeIcon.src = "images/button/dark_mode_FILL0_wght400_GRAD0_opsz24.svg"
+    themeIcon.src = "images/theme/moon-fill.svg"
   }
 }
