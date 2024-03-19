fe2dd96b9f37907af8a2d6af3ecf20b615feacbb
RedstoneParadox <spinjitzu00@gmail.com>
2024-03-19 09:25:07 -0700

    Polish up new darkmode switch
    Style and size changes

--- html/script/main.js
+++ html/script/main.js
@@ -2,7 +2,7 @@ function toggleDarkMode() {
   document.body.classList.toggle("dark")
   var themeIcon = document.querySelector(".theme-icon")
 
-  if (document.body.classList.contains("dark")) {
+  if (!document.body.classList.contains("dark")) {
     themeIcon.src = "images/button/light_mode_FILL0_wght400_GRAD0_opsz24.svg"
   } else {
     themeIcon.src = "images/button/dark_mode_FILL0_wght400_GRAD0_opsz24.svg"
