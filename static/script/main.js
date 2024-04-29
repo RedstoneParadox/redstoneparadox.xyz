9cb3dc1ae99c7f2954235e95ce695b2338a8c1c9
RedstoneParadox <spinjitzu00@gmail.com>
2024-04-28 21:31:42 -0700

    Converted theme toggle into web component

--- static/script/main.js
+++ static/script/main.js
@@ -1,10 +1,35 @@
-function toggleDarkMode() {
-  document.body.classList.toggle("dark")
-  var themeIcon = document.querySelector(".theme-icon")
+class ThemeToggle extends HTMLElement {
+  static observedAttributes = ["light-icon", "dark-icon"];
 
-  if (document.body.classList.contains("dark")) {
-    themeIcon.src = "images/theme/sun-fill.svg"
-  } else {
-    themeIcon.src = "images/theme/moon-fill.svg"
+  constructor() {
+    super();
+    this.icon = null
+  }
+
+  connectedCallback() {
+    const shadow = this.attachShadow({ mode: 'open' });
+    const button = document.createElement("button")
+    this.icon = document.createElement("img")
+
+    button.part = "button"
+    this.icon.part = "icon"
+    this.icon.src = this.getAttribute("dark-icon")
+
+    button.addEventListener("click", this.toggleTheme)
+
+    shadow.appendChild(button);
+    button.appendChild(this.icon)
+  }
+
+  toggleTheme() {
+    document.body.classList.toggle("dark")
+  
+    if (document.body.classList.contains("dark")) {
+      this.icon.src = this.getAttribute("light-icon")
+    } else {
+      this.icon.src = this.getAttribute("dark-icon")
+    }
   }
 }
+
+customElements.define("theme-toggle", ThemeToggle)
