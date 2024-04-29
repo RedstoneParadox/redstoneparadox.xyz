6d0d11c456311c4212428aa9c9fd9ef0da468bb6
RedstoneParadox <spinjitzu00@gmail.com>
2024-04-28 22:13:18 -0700

    ThemeToggle stores current theme in a cookie

--- static/script/main.js
+++ static/script/main.js
@@ -1,3 +1,27 @@
+function setCookie(cname, cvalue, exdays) {
+  const d = new Date();
+  d.setTime(d.getTime() + (exdays*24*60*60*1000));
+  let expires = "expires="+ d.toUTCString();
+  document.cookie = cname + "=" + cvalue + ";path=/";
+}
+
+function getCookie(cname) {
+  let name = cname + "=";
+  let decodedCookie = decodeURIComponent(document.cookie);
+  let ca = decodedCookie.split(';');
+  for(let i = 0; i <ca.length; i++) {
+    let c = ca[i];
+    while (c.charAt(0) == ' ') {
+      c = c.substring(1);
+    }
+    if (c.indexOf(name) == 0) {
+      return c.substring(name.length, c.length);
+    }
+  }
+  return "";
+}
+
+
 class ThemeToggle extends HTMLElement {
   static observedAttributes = ["light-icon", "dark-icon"];
 
@@ -14,7 +38,16 @@ class ThemeToggle extends HTMLElement {
 
     button.part = "button"
     this.icon.part = "icon"
-    this.icon.src = this.getAttribute("dark-icon")
+
+    const theme = getCookie("theme")
+
+    if (theme == "light") {
+      this.icon.src = this.getAttribute("dark-icon")
+      console.log("Here!");
+    } else if (theme == "dark") {
+      document.body.classList.toggle("dark")
+      this.icon.src = this.getAttribute("light-icon")
+    }
 
     button.addEventListener("click", function() {
       self.toggleTheme();
@@ -29,8 +62,10 @@ class ThemeToggle extends HTMLElement {
   
     if (document.body.classList.contains("dark")) {
       this.icon.src = this.getAttribute("light-icon")
+      setCookie("theme", "dark", 999)
     } else {
       this.icon.src = this.getAttribute("dark-icon")
+      setCookie("theme", "light", 999)
     }
   }
 }
